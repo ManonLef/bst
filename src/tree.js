4dba03164608c1c125107b0442d12e48a6ba0b74
Manon <manonlefkos@gmail.com>
2023-06-09 17:23:23 +0200

    add inital tree builder and prettyPrint function

--- src/tree.js
+++ src/tree.js
@@ -1,15 +1,13 @@
+import Node from "./node"
+
 class Tree {
   constructor(array) {
     this.root = buildTree(array);
   }
 }
 
-function buildTree(array) {
-
-}
-
 function preparedArray(array) {
-  const bstArray = mergeSort(removeDupl(array))
+  const bstArray = mergeSort(removeDupl(array));
   return bstArray;
 }
 
@@ -42,14 +40,36 @@ function mergeSort(array) {
 }
 
 function removeDupl(array) {
-  let arrayCopy = [...array]
-  let filteredArray = [...new Set(arrayCopy)]
-  return filteredArray
+  let arrayCopy = [...array];
+  let filteredArray = [...new Set(arrayCopy)];
+  return filteredArray;
 }
 
+const testArray = [1, 7, 4, 23, 8, 9, 4, 12, 25, 12, 3, 3, 3, 5, 7, 9, 67, 6345, 324]
+console.log(
+  preparedArray(testArray)
+); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
-console.log(preparedArray([9, 4, 6, 5, 7, 2, 2, 3, 8, 1, 13, 9, 10, 12, 14, 11, 15])); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+console.log(buildTree(preparedArray(testArray)))
 
+function buildTree(array) {
+
+  // base
+  if (array.length === 0) {
+    return null;
+  }
+
+  const mid = Math.floor(array.length / 2);
+  const root = new Node(array[mid])
+
+  // left half
+  root.left = buildTree(array.slice(0, mid-1))
+  
+  // right half
+  root.right = buildTree(array.slice(mid+1, array.length))
+
+  return root
+}
 
 // building the tree function pseudo
 // sort array and remove duplicates (check sorted array assignment)
@@ -62,3 +82,18 @@ console.log(preparedArray([9, 4, 6, 5, 7, 2, 2, 3, 8, 1, 13, 9, 10, 12, 14, 11, 
 
 // find mid point of right half of the array
 // mid point right half will be the right child of initial root
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+prettyPrint(buildTree(preparedArray(testArray)))
